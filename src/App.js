985d7a7b3d7603f5f9588b3ae8d163333e670c12
mamume <mmustafametwally@gmail.com>
2021-11-09 00:12:25 +0200

    Adding Component Files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import Header from './components/Header'
+import inputForm from './components/inputForm'
+import meme from './components/meme'
 
 function App() {
   return (
-    <div className="App">
+    <div>
     </div>
   );
 }
